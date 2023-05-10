e23f7a83e5bc84b677fe84d7e973d2ad8a7f2ce2
Alina Tarnakova <alina777pskov@mail.ru>
2023-05-10 07:46:40 +0300

    remove ||

--- config.js
+++ config.js
@@ -2,8 +2,7 @@
 
 // PORT='3000'
 
-const { PORT = 3000 } = process.env.PORT || 3001;
-const { BD = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
+const { PORT = 3000, BD = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 module.exports = {
   PORT,
